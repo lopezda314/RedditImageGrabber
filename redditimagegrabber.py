68319708379aa11cde79f88bcd78d751a152fd62
lopezda314 <lopezda314@gmail.com>
2017-03-01 19:49:18 -0600

    cleanup of OOP principles

--- redditimagegrabber.py
+++ redditimagegrabber.py
@@ -1,3 +1,13 @@
+#####################################################################################
+#####################################################################################
+##    One of the first "useful" things I ever coded.                               ##
+##    Takes in subreddit and crawls thumbnails using Reddit's REST API.            ##
+##    TODO - add pagination                                                        ##
+##    Main Features - RedditCrawl Class (does all the hardwork)                    ##
+##   - process_images does all the image processing for users that want image stuff## 
+#####################################################################################
+#####################################################################################
+
 #importing all the modules
 import requests
 import json
@@ -8,6 +18,7 @@ from skimage import io
 from PIL import Image
 import argparse
 
+#searches JSON for specified key/value pairs and stores result in pics
 def get_all(myjson, key, pics):
 	if type(myjson) == str:
 		myjson = json.loads(myjson)
@@ -29,50 +40,63 @@ def get_avg(img):
 	average_color = numpy.uint8(average_color)
 	return average_color
 
-#set username and password values
-username = input("Give Me Your Username!:")
-password = getpass.getpass()
+def process_images(data, colors):
+    for index, link in enumerate(data):
+        if link[0:4] == 'http':
+            image_file = io.imread(link)
+            colors[index][0] = get_avg(image_file[:][:][0])
+            colors[index][1] = get_avg(image_file[:][:][1])
+            colors[index][2] = get_avg(image_file[:][:][2])
+            print (avg_subreddit[index])
+            time.sleep(1.5)
 
-subreddit = str(input('Place the subreddit you want to know about here:'))
-#create dict with username and password
-user_pass_dict = {'user': username,
-				  'passwd': password,
-				  'api_type': 'json',}
+    average_color = numpy.average(colors, axis=0)
+    average_color_image = numpy.array([[average_color]*100] * 100, numpy.uint8)
 
-#set the header for all the following requests
-headers = {'user-agent': 'image-grabber', }
+    Image.fromarray(average_color_image).save("output.jpg")
+    return
 
-#create a requests.session that'll handle our cookies for us
-client = requests.session()
-client.headers=headers
+class RedditCrawler():
+    def __init__(self, subreddit, user_dict, content_pull):
+        self.subreddit = subreddit
+        self.user_dict = user_dict
+        self.content_pull = content_pull
+        self.data = None
+        return
 
-#make a login request, passing in the user and pass as data
-r = client.post(r'http://www.reddit.com/r/' + subreddit + '/.json', data=user_pass_dict)
-pics = []
-iteration = get_all(r.text, 'thumbnail', pics)
-size = len(iteration)
-avg_subreddit = numpy.zeros((size, 3))
 
-for index, link in enumerate(iteration):
-	if link[0:4] == 'http':
-		image_file = io.imread(link)
-		avg_subreddit[index][0] = get_avg(image_file[:][:][0])
-		avg_subreddit[index][1] = get_avg(image_file[:][:][1])
-		avg_subreddit[index][2] = get_avg(image_file[:][:][2])
-		print (avg_subreddit[index])
-		time.sleep(1.5)
+    def crawl(self, headers={'user-agent':'image-grabber',}):
+        client = requests.session()
+        client.headers = headers
 
-average_color = numpy.average(avg_subreddit, axis=0)
-average_color_image = numpy.array([[average_color]*100] * 100, numpy.uint8)
+        r = client.post(r'http://ww.reddit.com/r/' + self.subreddit + '/.json', data=self.user_dict)
 
-Image.fromarray(average_color_image).save("output.jpg")
+        #use tempStorage for get_all method. all the content actually ends up in self.data though
+        tempStorage = []
+        self.data = get_all(r.text, self.content_pull, tempStorage)
+        return self.data
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("square", help="display a square of a given number",
-                        type=int)
-    args = parser.parse_args()
-    print(args.square**2)
+    parser = argparse.ArgumentParser(description="Use this program to crawl Reddit for useful info like thumbnails or comments of your favorite Subreddit!")
+    parser.add_argument("-u", help="Your Reddit Username", required=True)
+    parser.add_argument("-p", help="Your Reddit Password", required=True)
+    parser.add_argument("-s", "--subreddit", help="Subreddit you want to crawl", required=True)
+    args = vars(parser.parse_args())
+    if len(args) != 3:
+        quit()
 
+    #set username and password values
+    username = args['u']
+    password = args['p']
+    subreddit = args['subreddit']
+    #create dict with username and password
+    user_pass_dict = {'user': username,
+                     'passwd': password,
+                    'api_type': 'json',}
+
+    redditcrawl = RedditCrawler(subreddit, user_pass_dict, 'thumbnail')
+    pics = redditcrawl.crawl()
+    avg_subreddit = numpy.zeros((len(pics), 3))
+    process_images(pics, avg_subreddit)
